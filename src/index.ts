7f9a1a18f9d1982fdfd56ee411a051e16a5e56b8
yuuki-katsuta <reptpr0314@gmail.com>
2022-02-20 12:24:18 +0900

    対話用の関数を作成

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-const sayHello = (name: string) => {
-  return `hello, ${name}!!`;
+const printLine = (text: string, breakLine: boolean = true) => {
+  process.stdout.write(text + (breakLine ? '\n' : ''));
 };
 
-process.stdout.write(sayHello('yuuki'));
+const promptInput = async (text: string) => {
+  printLine(`\n${text}\n>`, false);
+  //標準入力を1回だけ受け取る
+  const input: string = await new Promise((resolve) =>
+    process.stdin.once('data', (data) => resolve(data.toString()))
+  );
+  return input.trim();
+};
+
+(async () => {
+  const name = await promptInput('名前を入力してね!');
+  console.log(name);
+  const age = await promptInput('年齢を教えてよ〜');
+  console.log(age);
+  process.exit();
+})();
